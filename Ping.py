da0de904bc4bd2b6816e18debdda5125eefafcac
Rafail Sharifov <68757056+rafailsharifov@users.noreply.github.com>
2021-11-05 11:54:34 +0400

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -10,7 +10,7 @@ import os, platform
 
 time_count = 1
 command_status = True
-phone_number = [994513228785]
+phone_number = [99451123456,99450987654321]
 
 
 try:
@@ -24,9 +24,9 @@ try:
                 response = os.system("ping -W 1 " + ("-n 1 " if platform.system().lower() == "windows" else "-c 1 ") + host)
                 now = datetime.now()
                 # and then check the response...
-                if host == "85.132.61.82":
+                if host == "11.1.1.1":
                     hostname = "Delta"
-                elif host == "109.235.197.86":
+                elif host == "22.2.2.2":
                     hostname = "Azertelekom"
 
                 if response == 0:  # we got echo reply packet from partner
@@ -42,7 +42,7 @@ try:
                         temp_file.close()
                     except: sms(to=phone_number, text="Could not write log!\nSent from script on cloud!")
 
-            if (f / i) * 100 > 40:  # If loss is greater than 40 percent
+            if (f / i) * 100 > 60:  # If loss is greater than 40 percent
                 time_count += 1
 
                 if time_count == 3:  # Retry 2 times to ensure Delta is DOWN
@@ -59,19 +59,21 @@ try:
             if command_status is False:  # If is True, it means last time up_commands was sent, so it is working over Delta right now
                 sms(to=phone_number, text="ASB Internet connection over " + hostname + " is down!\nSent from script on cloud!")
                 command_status = True
+                
+             if datetime.today().day == 1:
+                if datetime.now().strftime("%H:%M") == "06:30":
+                    sms(to =phone_number, text = "It is 1st of month and I am always on.\nRegards,\nYour script on cloud:)")
+                    time.sleep(60)
 
+            if time_count == 6000000:
+                time_count = 60
 
-            if datetime.today().day == 1:
-                if datetime.now().strftime("%H:%M") == "10:30":
-                    sms(to =phone_number, text = "It is 1st of month and I am always on.\nRegards,\nYour script on cloud")
-
-        #result.put([hostname])
 
 
     if __name__ == '__main__':
 
         result = mp.Queue()
-        processes = [mp.Process(target=paralel_process, args=(host, result, time_count, command_status, phone_number)) for host in ["85.132.61.82", "109.235.197.86"]]
+        processes = [mp.Process(target=paralel_process, args=(host, result, time_count, command_status, phone_number)) for host in ["11.1.1.1", "22.2.2.2"]]
 
         for p in processes:
             p.start()
@@ -85,3 +87,5 @@ try:
 
 except Exception as text:
      sms(to = phone_number, text = "Ooouups,\nPing checkup failed!\nSent from script on cloud!")
+
+                                                                                                            
